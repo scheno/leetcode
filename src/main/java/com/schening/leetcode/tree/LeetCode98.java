fcfb9aea7166bdc75dd2388a5071af1698a3f80c
schening <schening@outlook.com>
2023-12-11 21:12:14 +0800

    LeetCode513. 找树左下角的值

---
+++ src/main/java/com/schening/leetcode/tree/LeetCode98.java
@@ -0,0 +1,16 @@
+package com.schening.leetcode.tree;
+
+import com.schening.leetcode.tree.traverse.TreeNode;
+
+/**
+ * LeetCode98. 验证二叉搜索树
+ *
+ * 题解：
+ */
+public class LeetCode98 {
+
+    public boolean isValidBST(TreeNode root) {
+
+    }
+
+}
